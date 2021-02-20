be735295c496760feb1d48713376f3034b9c8310
liuanguang <liuanguang@cvte.com>
2021-02-20 10:08:29 +0800

    chore: update datasource config

--- src/main/resources/db/create-table.sql
+++ src/main/resources/db/create-table.sql
@@ -1,3 +1,4 @@
+# DDL
 create table user
 (
     id          int auto_increment primary key,
@@ -8,3 +9,8 @@ create table user
     update_time datetime default CURRENT_TIMESTAMP not null,
     deleted     tinyint  default 0                 not null
 );
+
+# DATA
+INSERT INTO multi_datasource_master.user (id, username, password, sex) VALUES (1, 'master_username', 'master_password', 1);
+INSERT INTO multi_datasource_slave.user (id, username, password, sex) VALUES (1, 'slave_username', 'slave_password', 1);
+INSERT INTO multi_datasource_slave_2.user (id, username, password, sex) VALUES (1, 'slave_2_username', 'slave_2_password', 1);
